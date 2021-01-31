3214174597fc5ae443c8b747e35701b88140aded
elreydetoda <unc741@gmail.com>
2021-01-31 01:11:13 -0500

    example import statements

---
+++ groovy/basic_syntax/Test.groovy
@@ -0,0 +1,13 @@
+import groovy.xml.MarkupBuilder 
+def xml = new MarkupBuilder() 
+
+import java.lang.* 
+import java.util.* 
+import java.io.* 
+import java.net.* 
+
+import groovy.lang.* 
+import groovy.util.* 
+
+import java.math.BigInteger 
+import java.math.BigDecimal
