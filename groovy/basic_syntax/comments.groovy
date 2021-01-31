902cb9854056b9dbfbcc6ff207013b945ed9be61
elreydetoda <unc741@gmail.com>
2021-01-31 01:16:35 -0500

    comments example

---
+++ groovy/basic_syntax/comments.groovy
@@ -0,0 +1,14 @@
+class Example {
+   static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      println('Hello World');
+   }
+}
+
+class Example {
+   static void main(String[] args) {
+      /* This program is the first program
+      This program shows how to display hello world */
+      println('Hello World');
+   }
+}
