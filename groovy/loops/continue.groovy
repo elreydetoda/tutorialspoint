3e1ef62ff378bc0cd639ae4743ef7f0092d09658
elreydetoda <unc741@gmail.com>
2021-01-31 01:59:33 -0500

    loops

---
+++ groovy/loops/continue.groovy
@@ -0,0 +1,11 @@
+class Example {
+   static void main(String[] args) {
+      int[] array = [0,1,2,3];
+        
+      for(int i in array) {
+         if(i == 2)
+         continue;
+         println(i);
+      }
+   }
+}
---
+++ groovy/loops/for.groovy
@@ -0,0 +1,9 @@
+class Example { 
+   static void main(String[] args) {
+    
+      for(int i = 0;i<5;i++) {
+         println(i);
+      }
+        
+   }
+}
