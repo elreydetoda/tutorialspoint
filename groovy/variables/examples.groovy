800ecc73efe68f133c35fd89443e90579c7b2882
elreydetoda <unc741@gmail.com>
2021-01-31 01:53:33 -0500

    adding variables example

---
+++ groovy/variables/examples.groovy
@@ -0,0 +1,29 @@
+class Example { 
+   static void main(String[] args) { 
+      // x is defined as a variable 
+      String y = "Hello";
+        
+      // The value of the variable is printed to the console 
+      println(y);
+
+      // Defining a variable in lowercase  
+      int x = 5;
+      
+      // Defining a variable in uppercase  
+      int X = 6; 
+      
+      // Defining a variable with the underscore in it's name 
+      def _Name = "Joe"; 
+        
+      println(x); 
+      println(X); 
+      println(_Name); 
+
+      //Initializing 2 variables 
+      int r = 5; 
+      int R = 6; 
+      
+      //Printing the value of the variables to the console 
+      println("The value of x is " + r + "The value of X is " + R);
+   }
+}
