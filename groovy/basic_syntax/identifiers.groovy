a30d1924f7fc63fbe0aad6eb0b041af271c40158
elreydetoda <unc741@gmail.com>
2021-01-31 01:34:34 -0500

    extra info for intro groovy stuff

---
+++ groovy/basic_syntax/identifiers.groovy
@@ -0,0 +1,20 @@
+def employeename 
+def student1 
+def student_name
+
+class Example {
+   static void main(String[] args) {
+      // One can see the use of a semi-colon after each statement
+      def x = 5;
+      println('Hello World'); 
+   }
+}
+
+// whitespaces
+def x = 5;
+
+// literals
+12 
+1.45 
+‘a’ 
+“aa”
