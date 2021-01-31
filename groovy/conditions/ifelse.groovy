46cf72f24f8d055fb8e1df41e8288a6a9f8fd8e2
elreydetoda <unc741@gmail.com>
2021-01-31 02:04:49 -0500

    adding condition examples

---
+++ groovy/conditions/ifelse.groovy
@@ -0,0 +1,15 @@
+class Example { 
+   static void main(String[] args) { 
+      // Initializing a local variable 
+      int a = 2
+        
+      //Check for the boolean condition 
+      if (a<100) { 
+         //If the condition is true print the following statement 
+         println("The value is less than 100"); 
+      } else { 
+         //If the condition is false print the following statement 
+         println("The value is greater than 100"); 
+      } 
+   } 
+}
---
+++ groovy/conditions/ifnested.groovy
@@ -0,0 +1,21 @@
+class Example { 
+   static void main(String[] args) { 
+      // Initializing a local variable 
+      int a = 12 
+        
+      //Check for the boolean condition 
+      if (a>100) {
+         //If the condition is true print the following statement 
+         println("The value is less than 100"); 
+      } else 
+         // Check if the value of a is greater than 5 
+            
+      if (a>5) { 
+         //If the condition is true print the following statement 
+         println("The value is greater than 5 and greater than 100"); 
+      } else { 
+         //If the condition is false print the following statement 
+         println("The value of a is less than 5"); 
+      }  
+   } 
+}    
